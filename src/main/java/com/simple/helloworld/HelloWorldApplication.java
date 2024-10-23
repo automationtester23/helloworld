7a5de377a0e49cdd8683bc2ca13176f4cdf87be2
sumedh <sumedh@example.com>
2024-10-23 15:49:55 +0530

    first commit

---
+++ src/main/java/com/simple/helloworld/HelloWorldApplication.java
@@ -0,0 +1,15 @@
+package com.simple.helloworld;
+
+
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		new HelloWorldApplication().writeShell();
+	}
+
+	public void writeShell(){
+		System.out.println("Sumedh Shinde");
+	}
+
+
+}
